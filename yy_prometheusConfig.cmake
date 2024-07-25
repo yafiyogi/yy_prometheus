e61e509d9f19c1920c10abe0f6b5f7692fc3e34c
yafiyogi <1446544+yafiyogi@users.noreply.github.com>
2024-07-25 13:45:51 +0100

    Add cmake 'find_package()' config.

---
+++ yy_prometheusConfig.cmake
@@ -0,0 +1,5 @@
+include(CMakeFindDependencyMacro)
+find_dependency(yy_cpp 0.0.1)
+find_dependency(yy_json 0.0.1)
+find_dependency(yy_mqtt 0.0.1)
+include(${CMAKE_CURRENT_LIST_DIR}/yy_prometheusTargets.cmake)
